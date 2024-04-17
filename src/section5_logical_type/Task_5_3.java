1d1727401eaf6bebffa1aa92d1fcde176941198f
sosadwaden <wadenjap@gmail.com>
2024-04-17 22:21:24 +0300

    Сделан раздел 5

---
+++ src/section5_logical_type/Task_5_3.java
@@ -0,0 +1,28 @@
+package section5_logical_type;
+
+public class Task_5_3 {
+
+    public void action() {
+        /*
+            Задание: В приведенной ниже таблице сделайте для каждого выражения, команды
+            и блока (группы команд) словесные описания.
+            ------------------------------------------------------------------------------------------
+            Equal = (x == t);                               |  Equal равен (x равно t)
+
+            proverka = (a == b && t == z);                  |  proverka равен (a равно b и t равно z)
+
+            prover = true;                                  - prover равен правде
+            System.out.println("Результат =" + prover);     - вывести "Результат =" true
+            prover =! prover                                - prover = false
+            System.out.println("Результат =" + prover);     - вывести "Резльутат =" false
+            prover != prover;                               - prover = true
+            System.out.println("Реузльтат =" + prover);     - вывести "Результат =" true
+
+            check = num1 > num2;                            |  check равен num1 больше num2
+
+            result = ((a + b) > 100);                       |  result равен ((a + b) больше 100)
+
+            ok = ((a + b * 10 - 100) != 0);                 |  ok равен ((a + b * 10 - 100) не равно 0)
+         */
+    }
+}
